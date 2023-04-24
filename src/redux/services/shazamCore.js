28c143108ae44552dd45eb9e78029aa29324abe7
Prabesh77 <prabeshregmi17@gmail.com>
2023-04-24 11:56:06 +1000

    Song details, artist details and lyrics page

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -4,17 +4,25 @@ import { createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 export const shazamCoreApi = createApi({
     reducerPath: 'shazamCoreApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://shazam-core.p.rapidapi.com/v1/',
+        baseUrl: 'https://shazam-core.p.rapidapi.com/',
         prepareHeaders: (headers) => {
             headers.set('x-rapidapi-key', '11f3464d31mshe741827687476c4p109d89jsn5e7c6d3b6519')
             return headers
         }
     }),
     endpoints: (builder) => ({
-        getTopCharts: builder.query({query:() => '/charts/world'})
+        getTopCharts: builder.query({query:() => 'v1/charts/world'}),
+        getSongDetails: builder.query({ query: ({songid}) => `v1/tracks/details?track_id=${songid}`}),
+        getSongRelated: builder.query({ query: ({ songid }) => `v1/tracks/related?track_id=${songid}` }),
+        getArtistDetails: builder.query({ query: (artistId) => `v2/artists/details?artist_id=${artistId}` }),
+        getSongsByCountry: builder.query({query: (countryCode) => `v1/charts/country?country_code=${countryCode}`})
     })
 })
 
 export const {
-    useGetTopChartsQuery
+    useGetTopChartsQuery,
+    useGetSongDetailsQuery,
+    useGetSongRelatedQuery,
+    useGetArtistDetailsQuery,
+    useGetSongsByCountryQuery
 } = shazamCoreApi
